ff13c08e700774f4c0509a40a2839d70d0460254
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-09 10:04:38 +0530

    Day-1 completed

---
+++ Day1_PostAssignment/demo_1.java
@@ -0,0 +1,9 @@
+package my_dsa;
+
+public class demo_1 {
+	public static void main(String[]args)
+	{
+		System.out.println("Demo_1");
+	}
+
+}
---
+++ Day1_Task/demo_2.java
@@ -0,0 +1,8 @@
+package my_dsa;
+
+public class demo_2 {
+	public static void main(String[]args)
+	{
+		System.out.println("Demo_2");
+	}
+}
