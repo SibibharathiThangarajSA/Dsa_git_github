4ebaefd78236829a7c8b2b342cca36b824de6341
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-10 14:09:25 +0530

    task completed

---
+++ Day2_PostAssignment/findTwoSum_PostAssign.java
@@ -0,0 +1,42 @@
+package my_dsa2;
+import java.util.Arrays;
+public class findTwoSum_PostAssign
+{
+	 public static void main(String[] args)
+	 {
+	        int[] nums = {4, 5, 2, 6, 9, 10};
+	        int target = 8;
+	        int[] result = findTwoSum(nums, target);
+	        if (result != null)
+	        {
+	            System.out.println("Pair found: " + result[0] + ", " + result[1]);
+	        } 
+	        else
+	        {
+	            System.out.println("No pair found.");
+	        }
+	    }
+	public static int[] findTwoSum(int[] nums, int target)
+	{
+        Arrays.sort(nums);
+        int left = 0, right = nums.length - 1;
+        while (left < right)
+        {
+            int currentSum = nums[left] + nums[right];
+            if (currentSum == target)
+            {
+                return new int[] {nums[left], nums[right]
+                		};
+            } 
+            else if (currentSum < target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return null;
+    }
+}
